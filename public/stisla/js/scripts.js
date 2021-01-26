448eb59eb4212a93637a43714a6f368e38318e9b
masif088 <mokhamadasif@gmail.com>
2021-01-26 11:47:11 +0700

    fixing datetimepicker probelm 24 hour format

--- public/stisla/js/scripts.js
+++ public/stisla/js/scripts.js
@@ -591,7 +591,7 @@ $(function() {
     }
     if($(".datetimepicker").length) {
       $('.datetimepicker').daterangepicker({
-        locale: {format: 'YYYY-MM-DD hh:mm'},
+        locale: {format: 'YYYY-MM-DD HH:mm'},
         singleDatePicker: true,
         timePicker: true,
         timePicker24Hour: true,
